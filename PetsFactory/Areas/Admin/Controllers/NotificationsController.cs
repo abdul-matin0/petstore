9731a1e99f8c75c662bd5b3cc5cd88e0316bde8a
Abdul-matin <madeayo04@gmail.com>
2021-07-01 06:55:56 +0100

    notifications

---
+++ PetsFactory/Areas/Admin/Controllers/NotificationsController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PetsFactory.Data.Repository.IRepository;
+using PetsFactory.Models;
+using PetsFactory.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetsFactory.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = Utilities.AdminUser)]
+
+    public class NotificationsController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public NotificationsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        // view all notificaitons 
+        public IActionResult Index()
+        {
+            IEnumerable<Notifications> allNotifications = _unitOfWork.Notifications.GetAll();
+
+            return View(allNotifications);
+        }
+
+        // delete notification
+        #region API
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.Notifications.GetFirstOrDefault(u => u.Id == id);
+
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Something Went Wrong" });
+            }
+
+            _unitOfWork.Notifications.Remove(objFromDb);
+            _unitOfWork.Save();
+
+            return Json(new { success = true, message = "Deleted Successfully" });
+
+        }
+        #endregion
+    }
+}
--- PetsFactory/wwwroot/js/site.js
+++ PetsFactory/wwwroot/js/site.js
@@ -1,4 +1,34 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function Delete(url) {
+    // display sweet alert
 
-// Write your JavaScript code.
+    Swal.fire({
+        title: 'Are you sure you want to Delete?',
+        text: "You will not be able to restore the data!",
+        showCancelButton: true,
+        confirmButtonText: `Delete`
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data1) {
+                    if (data1.success) {
+                        Swal.fire(
+                            'Success!',
+                            data1.message,
+                            'success'
+                        ).then((btnOk) => {
+                            location.reload(true);
+                        });
+                    } else {
+                        Swal.fire(
+                            'Error!',
+                            data.message,
+                            'error'
+                        )
+                    }
+                }
+            });
+        }
+    });
+}
