7031371e5d2fddea027b96c60a10f0c8a3d3fc9c
Abdul-matin <madeayo04@gmail.com>
2021-07-05 08:26:25 +0100

    deployment to hosting

---
+++ PetsFactory/obj/Release/netcoreapp3.1/win-x86/PubTmp/Out/wwwroot/js/site.js
@@ -0,0 +1,34 @@
+﻿function Delete(url) {
+    // display sweet alert
+
+    Swal.fire({
+        title: 'Are you sure you want to Delete?',
+        text: "You will not be able to restore the data!",
+        showCancelButton: true,
+        confirmButtonText: `Delete`
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data1) {
+                    if (data1.success) {
+                        Swal.fire(
+                            'Success!',
+                            data1.message,
+                            'success'
+                        ).then((btnOk) => {
+                            location.reload(true);
+                        });
+                    } else {
+                        Swal.fire(
+                            'Error!',
+                            data.message,
+                            'error'
+                        )
+                    }
+                }
+            });
+        }
+    });
+}
